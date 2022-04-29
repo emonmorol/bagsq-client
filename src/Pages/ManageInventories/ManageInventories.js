b0622cf8d54b3d4c55118ed123b80aaf81e7ef47
emonmorol <ekramulhasane69@gmail.com>
2022-04-30 05:25:17 +0600

    used delete api to delete item from table

--- src/Pages/ManageInventories/ManageInventories.js
+++ src/Pages/ManageInventories/ManageInventories.js
@@ -4,65 +4,81 @@ import React from "react";
 import useProducts from "../../Hooks/useProducts";
 
 const ManageInventories = () => {
-  const [products] = useProducts([]);
+  const [products, setProducts] = useProducts([]);
+  const handleDelete = (id) => {
+    const proceed = window.confirm("Are Your Sure? You want To Delete??");
+    if (proceed) {
+      const url = `http://localhost:5000/inventory/${id}`;
+      fetch(url, {
+        method: "DELETE",
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          const remainingProducts = products.filter(
+            (product) => product._id !== id
+          );
+          setProducts(remainingProducts);
+        });
+    }
+  };
 
   return (
     <div className="px-[10%] py-[5%]">
-      <h2 className="text-center my-3">All Inventories</h2>
-      <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
-        <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-          <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+      <h2 className="text-center my-3">All Inventories {products.length}</h2>
+      <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
+        <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+          <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
             <tr>
-              <th scope="col" class="px-8 py-3  border-x text-center">
+              <th scope="col" className="px-8 py-3  border-x text-center">
                 Product Image
               </th>
-              <th scope="col" class="px-6 py-3  border-x text-center">
+              <th scope="col" className="px-6 py-3  border-x text-center">
                 Product name
               </th>
-              <th scope="col" class="px-6 py-3  border-x text-center">
+              <th scope="col" className="px-6 py-3  border-x text-center">
                 Price
               </th>
-              <th scope="col" class="px-6 py-3  border-x text-center">
+              <th scope="col" className="px-6 py-3  border-x text-center">
                 Quantity
               </th>
-              <th scope="col" class="px-6 py-3  border-x text-center">
+              <th scope="col" className="px-6 py-3  border-x text-center">
                 Supplier
               </th>
-              <th scope="col" class="px-6 py-3  border-x text-center">
-                <span class="sr-only">Control</span>
+              <th scope="col" className="px-6 py-3  border-x text-center">
+                <span className="sr-only">Control</span>
               </th>
             </tr>
           </thead>
           <tbody>
             {products.map((product) => (
-              <>
-                <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-                  <td class="w-1/12 px-6 py-3 border-x  dark:text-white whitespace-nowrap">
-                    <img
-                      className="w-1/2 mx-auto"
-                      src={product?.image}
-                      alt=""
-                    />
-                  </td>
-                  <td class="px-6 py-3 border-x text-center font-medium text-gray-900 dark:text-white whitespace-nowrap">
-                    {product?.name}
-                  </td>
-                  <td class="px-6 py-3 border-x text-center">
-                    ${product?.price}
-                  </td>
-                  <td class="px-6 py-3 border-x text-center">
-                    {product?.quantity}
-                  </td>
-                  <td class="px-6 py-3 border-x text-center">
-                    {product?.supplier}
-                  </td>
-                  <td class="px-6 py-3 border-x text-center text-red-500 cursor-pointer">
-                    <button className="bg-red-200 py-2 px-3 text-base rounded-full">
-                      <FontAwesomeIcon icon={faTrash} />
-                    </button>
-                  </td>
-                </tr>
-              </>
+              <tr
+                key={product._id}
+                className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600"
+              >
+                <td className="w-1/12 px-6 py-3 border-x  dark:text-white whitespace-nowrap">
+                  <img className="w-1/2 mx-auto" src={product?.image} alt="" />
+                </td>
+                <td className="px-6 py-3 border-x text-center font-medium text-gray-900 dark:text-white whitespace-nowrap">
+                  {product?.name}
+                </td>
+                <td className="px-6 py-3 border-x text-center">
+                  ${product?.price}
+                </td>
+                <td className="px-6 py-3 border-x text-center">
+                  {product?.quantity}
+                </td>
+                <td className="px-6 py-3 border-x text-center">
+                  {product?.supplier}
+                </td>
+                <td className="px-6 py-3 border-x text-center text-red-500 cursor-pointer">
+                  <button
+                    onClick={() => handleDelete(product._id)}
+                    className="bg-red-200 py-2 px-3 text-base rounded-full"
+                  >
+                    <FontAwesomeIcon icon={faTrash} />
+                  </button>
+                </td>
+              </tr>
             ))}
           </tbody>
         </table>
