145a7927069d4929cf10a80ad02fe01a39abaf2e
emonmorol <ekramulhasane69@gmail.com>
2022-04-30 20:11:34 +0600

    my item page added

--- src/Pages/MyItem/MyItem.js
+++ src/Pages/MyItem/MyItem.js
@@ -1,9 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { Link } from "react-router-dom";
+import auth from "../../fireabase.init";
+import MyItemCard from "./MyItemCard";
 
 const MyItem = () => {
+  const [user] = useAuthState(auth);
+  const [myInventory, setMyInventory] = useState([]);
+
+  useEffect(() => {
+    const url = `http://localhost:5000/myitem?email=${user?.email}`;
+    axios(url).then((response) => {
+      setMyInventory(response.data);
+    });
+  }, [user]);
+
   return (
-    <div>
-      <h1>This is my item page</h1>
+    <div className="py-14 flex flex-col justify-center">
+      <h1 className="text-center">Our Products</h1>
+      <div className="container mx-auto grid grid-cols-1 lg:grid-cols-2 gap-5 lg:px-28 pt-5">
+        {myInventory.map((item) => (
+          <MyItemCard key={item._id} item={item} />
+        ))}
+      </div>
+      <Link
+        className="bg-teal-100 hover:bg-blue-300 py-1 w-1/6 mx-auto my-3 rounded text-center"
+        to="/manageInventories"
+      >
+        Manage Inventories
+      </Link>
     </div>
   );
 };
---
+++ src/Pages/MyItem/MyItemCard.js
@@ -0,0 +1,66 @@
+import { faStar, faStarHalfStroke } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React from "react";
+import { useNavigate } from "react-router-dom";
+// import useProducts from "../../Hooks/useProducts";
+
+const MyItemCard = ({ item }) => {
+  const { name, description, image, quantity, price, _id } = item;
+  const navigate = useNavigate();
+
+  return (
+    <div className="py-5 border rounded-2xl">
+      <div className="lg:flex block items-center">
+        <div className="lg:w-1/4  px-3 py-4 mx-auto">
+          <img src={image} alt="" />
+        </div>
+        <div className="lg:pr-20">
+          <div className="py-4 px-4 bg-white">
+            <span className="flex justify-between py-2">
+              <span className="block text-lg text-gray-600 font-bold tracking-wide">
+                {name}
+              </span>
+              ``
+              <span className="font-semibold uppercase text-blue-400">
+                ${price}
+              </span>
+            </span>
+            <span className="block text-gray-600 text-sm">
+              {description.slice(0, 50)}...
+            </span>
+
+            <div className="flex justify-between pt-1">
+              <span>
+                <span>Rating : </span>{" "}
+                <span className="text-blue-400">
+                  <FontAwesomeIcon icon={faStar} />
+                  <FontAwesomeIcon icon={faStar} />
+                  <FontAwesomeIcon icon={faStar} />
+                  <FontAwesomeIcon icon={faStar} />
+                  <FontAwesomeIcon icon={faStarHalfStroke} />
+                </span>
+              </span>
+              <span>Available: {quantity} Pcs.</span>
+            </div>
+          </div>
+          <div className="flex">
+            <button
+              onClick={() => navigate(`/inventory/${_id}`)}
+              className="w-1/2 mx-4 bg-blue-200 py-2 hover:bg-blue-400"
+            >
+              Manage
+            </button>
+            <button
+              onClick={() => handleDelete(_id)}
+              className="w-1/2 mx-4 bg-red-200 py-2 hover:bg-blue-400"
+            >
+              Delete
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default MyItemCard;
